87d856bb3caaf5e550ef377b0621ba4d4f5a4ab7
liaojx <dev-liaojianxin@example.com>
2025-04-10 17:11:18 +0800

    refactor: 优化请求工具和调整路由基础路径
    - 移除请求工具中多余的 `url` 参数，简化请求配置
    - 将路由基础路径从 `/unibest/` 更改为 `/`，使其更通用

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -51,14 +51,12 @@ const http = <T>(options: CustomRequestOptions) => {
  * openapi-ts-request 工具的 request 跨客户端适配方法
  */
 export default function request<T = unknown>(
-  url: string,
-  options: Omit<CustomRequestOptions, 'url'> & {
+  options: CustomRequestOptions & {
     params?: Record<string, unknown>
     headers?: Record<string, unknown>
   },
 ) {
   const requestOptions = {
-    url,
     ...options,
   }
 
